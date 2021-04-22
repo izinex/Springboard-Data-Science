76bb38c5f4dacbb5cf20adf4799fa0dd003a98fd
Pizon Shetu <izineox@yahoo.com>
2021-04-22 12:58:34 -0400

    Update SQLTasks Tier 2.sql

--- SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -35,8 +35,15 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
+SELECT membercost FROM Facilities
+WHERE membercost > 0.0;
 
 /* Q2: How many facilities do not charge a fee to members? */
+
+SELECT COUNT(membercost) FROM Facilities
+WHERE membercost > 0.0;
+
+5 Facilities do not charge a fee to members.
 
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
@@ -44,9 +51,15 @@ where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance FROM Facilities
+WHERE membercost < (monthlymaintenance * 0.20)
+
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
+
+SELECT * FROM Facilities
+WHERE facid IN (1, 5);
 
 
 /* Q5: Produce a list of facilities, with each labelled as
@@ -54,15 +67,40 @@ Try writing the query without using the OR operator. */
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT *,
+CASE WHEN monthlymaintenance > 100 THEN 'expensive'
+	ELSE 'cheap' END AS Price_Range
+FROM Facilities
+
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
+
+SELECT firstname, surname FROM Members
+WHERE joindate = (SELECT max(joindate) from Members)
 
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
+
+SELECT DISTINCT CONCAT(M.firstname,' ', M.surname) as MemberName, 
+F.name as Court_Name
+
+FROM Bookings as B
+
+INNER JOIN Facilities as F
+
+ON B.facid = F.facid
+
+INNER JOIN Members as M
+
+ON B.memid = M.memid
+
+WHERE F.facid in (0,1)
+
+ORDER BY MemberName
 
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
@@ -71,6 +109,8 @@ different costs to members (the listed costs are per half-hour 'slot'), and
 the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
+
+
 
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
